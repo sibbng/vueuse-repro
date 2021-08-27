f34455f9bc50aba48deafdf855fcad350639ae4d
sibbng <sibbngheid@gmail.com>
2021-08-27 21:09:56 +0300

    first commit

---
+++ nuxt/pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    {{x}} {{y}}
+  </div>
+</template>
+
+<script lang="ts">
+import { useMouse } from '@vueuse/core'
+
+export default {
+  setup() {
+    const { x, y } = useMouse()
+
+    return { x, y }
+  }
+}
+</script>
